1ae0591b01636232e7c9cf97386cf068f78896fa
Bryn Bayliss <brynbayliss@hotmail.com>
2017-10-30 18:29:33 +0000

    DRYed up profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,15 +1,17 @@
 class ProfilesController < ApplicationController
+  before_action :owned_profile, only: [:edit, :update]
+  before_action :authenticate_user!
+  before_action  :set_user
+
+
   def show
-    @user = User.find_by(user_name: params[:user_name])
     @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
   end
 
   def edit
-    @user = User.find_by(user_name: params[:user_name])
   end
 
   def update
-    @user = User.find_by(user_name: params[:user_name])    
     @user.update(profile_params)
     if @user.save
       flash[:success] = 'Your profile has been updated'
@@ -25,5 +27,17 @@ class ProfilesController < ApplicationController
 
   def profile_params
     params.require(:user).permit(:avatar, :bio)
+  end
+
+  def owned_profile 
+    @user = User.find_by(user_name: params[:user_name])
+    unless current_user == @user
+      flash[:alert] = "This isn't you profile!"
+      redirect_to root_path
+    end
+  end
+
+  def set_user
+    @user = User.find_by(user_name: params[:user_name])
   end
 end
