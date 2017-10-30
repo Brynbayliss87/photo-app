ec63b9d5f29778f9bec68b20c0e9a9ff0a686d60
Bryn Bayliss <brynbayliss@hotmail.com>
2017-10-30 17:08:15 +0000

    fixed bug in edit view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,6 +9,21 @@ class ProfilesController < ApplicationController
   end
 
   def update
-       
+    @user = User.find_by(user_name: params[:user_name])    
+    @user.update(profile_params)
+    if @user.save
+      flash[:success] = 'Your profile has been updated'
+      redirect_to profile_path(@user.user_name)
+    else
+      @user.errors.full_messages
+      flash[:error] = @user.errors.full_messages
+      render :edit
+    end
+  end
+
+  private 
+
+  def profile_params
+    params.require(:user).permit(:avatar, :bio)
   end
 end
