6336d0956d0ea4d8da76acf496e7ca834df3caad
Bryn Bayliss <brynbayliss@hotmail.com>
2017-10-31 16:03:23 +0000

    finished ld like funcrionality but jquery not working to refresh like list

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -10,10 +10,28 @@ module PostsHelper
     end
   end
 
+  def liked_post(post)
+    return 'glyphicon-heart' if current_user.voted_for? post
+    'glyphicon-heart-empty'
+  end
+
+  def display_likes(post)
+    votes = post.votes_for.up.by_type(User)
+    if votes.size <= 8
+      likers_of(post)
+    else
+      count_likers(votes)
+    end
+  end
   private
 
   def like_plural(votes)
     return ' like this' if votes.count > 1
     ' likes this'
+  end
+
+  def count_likers(votes)
+    vote_count = votes.size
+    vote_count.to_s + " likes"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  acts_as_voter
   has_attached_file :avatar, styles: { medium: '152x152#' }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*Z/
   has_many :comments, dependent: :destroy
